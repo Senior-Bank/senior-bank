d0a75d8ea3abc6ab5f6efa374d079f309314d495
Sarah Paula <79868811+Sarahpaula@users.noreply.github.com>
2025-11-13 21:43:00 -0300

    Criação do js nas paginas de index e pagar contas
    Js no botao de ocultar saldo,
    cancelar pagamento de contas, organizacao no css das mesmas

--- Senior-bank/pagar-contas.js
+++ Senior-bank/pagar-contas.js
@@ -3,13 +3,16 @@ const btnConfirmarPagamento = document.querySelector('.confirmar-pagamento');
 const popup = document.querySelector('.box-seguranca');
 const fechar = document.querySelector('.img-x');
 const body = document.getElementById('body');
+const btncancelar = document.getElementById('cancelarModal');
+const btnFinalizarPagamento = document.getElementById('confirmar');
+const verMaisContas = document.querySelector('.verMais')
+const divContas = document.querySelector('.contas')
 
 
 
 function abrirPopup() {
         popup.style.display = 'block';
-       body.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
-        console.log('Popup aberto!');
+        body.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
 
 }
 
@@ -17,11 +20,45 @@ function fecharPopup() {
         popup.style.display = 'none'
         body.style.backgroundColor = 'white';
 
+}
 
 
+function verContas(){
+
+        
+                
+        const div = document.createElement('div');
+        div.classList.add('conta')
+        div.innerHTML= ` 
+        <img src="./images/luz.svg" alt="icon de casa" div="img-luz">
+
+        <div class="container-textoInfo-contas">
+          <h1>Conta de telefone</h1>
+          <h2>Vencimento: 28/11/2025</h2>
+        </div>
+
+        <div class="valor">
+          <P>R$100,00</P>
+        </div>
+
+        <input type="checkbox" class="check">
+
+        `
+        divContas.appendChild(div)
+         
 }
 
 btnConfirmarPagamento.addEventListener('click',  abrirPopup);
 
-fechar.addEventListener('click', fecharPopup)
+fechar.addEventListener('click', fecharPopup);
+
+btncancelar.addEventListener('click', fecharPopup);
+
+verMaisContas.addEventListener('click', verContas);
+
+
+
+
+
+
 
