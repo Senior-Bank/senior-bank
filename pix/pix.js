7c53bb065f915368ded7c8707e2a37f6f300a301
Brenda Moreno <33201726+brendocaz@users.noreply.github.com>
2025-12-02 10:49:49 -0300

    atualizações-setinha
    -setas alteradas
    -tela pix
    -extrato

---
+++ pix/pix.js
@@ -0,0 +1,15 @@
+const form = document.getElementById('pix-form'); // Pegando o input da chave
+const valorInput = document.getElementById('valor-pix');   // Pegando o input do valor
+const modal = document.getElementById('modal-confirmacao');
+const btnCancelar = document.getElementById('btn-cancelar');
+const btnConfirmar = document.getElementById('btn-confirmar');
+
+     const dadosDestinatarios = {
+        '11999999999': { nome: 'Joana Silva Oliveira', banco: 'Banco SB (Sênior)' },
+        '12345678900': { nome: 'Nicollas Barbosa', banco: 'Banco do Brasil' },
+        'a4b5c6d7e8f9': { nome: 'Marcela Morais', banco: 'Itaú Unibanco' },
+    };
+
+document.addEventListener("click", {
+   
+})
